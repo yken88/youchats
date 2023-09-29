9d3176631f0ebccf4ffcc69d205a4d01126cea8e
yken88 <yken1019@icloud.com>
2023-09-30 00:35:16 +0900

    add streamers files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StreamersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', function () {
-    return 'aaa';
-});
+Route::resource('streamers', StreamersController::class);
